b2217d7fa5344e881629cd121904bd8a8988e10a
mpiechot <marco.piechotta@gmail.com>
2025-02-16 15:21:42 +0100

    Add Player Parry + Tilemap + NavMesh Stuff

--- BrackeysGameJam2025/Assets/Player/Player.cs
+++ BrackeysGameJam2025/Assets/Player/Player.cs
@@ -1,12 +1,35 @@
-﻿using UnityEngine;
+﻿using Assets.Porjectile;
+using System.Linq;
+using UnityEngine;
 
 namespace GameJam.Player
 {
     public class Player : MonoBehaviour, IUnit
     {
+        [SerializeField]
+        private float parryRange = 1f;
+
+        [SerializeField]
+        private float parryCooldown = 1f;
+
+        private bool canParry = true;
+        private bool isParrying = false;
+        private float cooldown = 0.0f;
+        private float parryTime = 0.2f;
+
         public void GetHit(int damage)
         {
             throw new System.NotImplementedException();
+        }
+
+        public void Parry()
+        {
+            var bulletsInRange = Physics2D.OverlapCircleAll(transform.position, parryRange).OfType<ProjectileBase>();
+
+            foreach (var bullet in bulletsInRange)
+            {
+                bullet.Parry(transform);
+            }
         }
     }
 }
---
+++ BrackeysGameJam2025/Assets/Porjectile/ProjectileBase.cs
@@ -0,0 +1,72 @@
+﻿using GameJam.Player;
+using UnityEngine;
+
+namespace Assets.Porjectile
+{
+    public class ProjectileBase : MonoBehaviour
+    {
+        [SerializeField]
+        private bool isFollowingTarget = false;
+
+        [SerializeField]
+        private float bulletSpeed = 1.0f;
+
+        private int bulletLifetime;
+        private int initialLifetime;
+        private int bulletDamage;
+        private Transform bulletSender;
+        private Transform bulletTarget;
+        private Vector3 bulletMoveVector;
+
+        private bool isDestroyed = false;
+
+        public void Fire(Transform sender, Transform targetInstance, int damage, int lieftime)
+        {
+            bulletSender = sender;
+            bulletMoveVector = (bulletTarget.position - transform.position).normalized;
+            bulletTarget = targetInstance;
+            bulletDamage = damage;
+            bulletLifetime = lieftime;
+            initialLifetime = bulletLifetime;
+        }
+
+        public void Parry(Transform newSender)
+        {
+            bulletTarget = bulletSender;
+            bulletSender = newSender;
+            bulletMoveVector = (bulletTarget.position - transform.position).normalized;
+            bulletLifetime = initialLifetime;
+        }
+
+        private void Update()
+        {
+            if (!isDestroyed && bulletLifetime <= 0)
+            {
+                ProjectileDie();
+                return;
+            }
+
+            if (isFollowingTarget)
+            {
+                bulletMoveVector = (bulletTarget.position - transform.position).normalized;
+            }
+
+            transform.position += bulletSpeed * Time.deltaTime * bulletMoveVector;
+            bulletLifetime--;
+        }
+
+        private void ProjectileDie()
+        {
+            Destroy(this.gameObject);
+            isDestroyed = true;
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.TryGetComponent<Player>(out var player))
+            {
+                player.GetHit(bulletDamage);
+            }
+        }
+    }
+}
